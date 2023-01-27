0a2d7bba58370827417a786406c2800c229d9fe7
오제웅 <jeung@docfriends.com>
2023-01-27 17:50:38 +0900

    반환 타입 진행중

--- src/main/java/com/example/datajpa/repository/MemberRepository.java
+++ src/main/java/com/example/datajpa/repository/MemberRepository.java
@@ -1,12 +1,30 @@
 package com.example.datajpa.repository;
 
+import com.example.datajpa.dto.MemberDto;
 import com.example.datajpa.entity.Member;
-import com.example.datajpa.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-/**
- * 인터페이스인데 기본 등록, 수정, 삭제, count 등 기능을 제공해준다.
- * 즉 JPA가 인터페이스를 바탕으로 구현 클래스를 만들어 Injection 해준다.
- */
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+/** 인터페이스인데 기본 등록, 수정, 삭제, count 등 기능을 제공해준다. 즉 JPA가 인터페이스를 바탕으로 구현 클래스를 만들어 Injection 해준다. */
 public interface MemberRepository extends JpaRepository<Member, Long> {
+  @Query("select m from Member m where m.username = :username and m.age = :age")
+  Member findUser(@Param("username") String username, @Param("age") int age);
+
+  @Query("select m.username from Member m")
+  List<String> findUserNameList();
+
+  @Query(
+      "select new com.example.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+  List<MemberDto> findMemberDto();
+
+  @Query("select m.username from Member m where m.username in :names")
+  List<String> findByNames(@Param("names") Collection<String> names);
+
+  //Optional<Member> findOptionalByUsername
 }
--- src/test/java/com/example/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/datajpa/repository/MemberRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.example.datajpa.repository;
 
+import com.example.datajpa.dto.MemberDto;
 import com.example.datajpa.entity.Member;
+import com.example.datajpa.entity.Team;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +16,38 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Transactional
 class MemberRepositoryTest {
   @Autowired MemberRepository memberRepository;
+  @Autowired TeamRepository teamRepository;
+
+  @Test
+  void selectUser() {
+    //given
+    Member user1 = new Member("ohjeung1", 22);
+    Member user2 = new Member("ohjeung2", 23);
+    memberRepository.save(user1);
+    memberRepository.save(user2);
+
+    //when
+    Member findUser = memberRepository.findUser(user1.getUsername(), user1.getAge());
+
+    //then
+    assertThat(findUser.getAge()).isEqualTo(22);
+    assertThat(findUser.getUsername()).isEqualTo("ohjeung1");
+  }
+
+  @Test
+  void selectUserNameList() {
+    //given
+    Member user1 = new Member("ohjeung1", 22);
+    Member user2 = new Member("ohjeung2", 23);
+    memberRepository.save(user1);
+    memberRepository.save(user2);
+
+    //when
+    List<String> userNameList = memberRepository.findUserNameList();
+
+    // then
+    System.out.println("userNameList = " + userNameList);
+  }
 
   @Test
   public void testMember() {
@@ -53,4 +87,40 @@ class MemberRepositoryTest {
     long deletedCount = memberRepository.count();
     assertThat(deletedCount).isEqualTo(0);
   }
+
+  @Test
+  void selectMemberDtoTest() {
+    // given
+    Team team = new Team("내마음대로팀");
+    teamRepository.save(team);
+
+    Member user1 = new Member("ohjeung1", 22);
+    user1.setTeam(team);
+    memberRepository.save(user1);
+
+    // when
+    List<MemberDto> memberDto = memberRepository.findMemberDto();
+
+    // then
+    System.out.println("memberDto = " + memberDto);
+    assertThat(memberDto.get(0)).isEqualTo(new MemberDto(2L, "ohjeung1", "내마음대로팀"));
+  }
+
+  @Test
+  void findByNamesTest() {
+    //given
+    Member user1 = new Member("ohjeung1", 22);
+    Member user2 = new Member("ohjeung2", 23);
+    memberRepository.save(user1);
+    memberRepository.save(user2);
+
+    //when
+    List<String> byNames = memberRepository.findByNames(List.of("ohjeung1", "ohjeung2"));
+
+    //then
+    assertThat(byNames.get(0)).isEqualTo("ohjeung1");
+    assertThat(byNames.get(1)).isEqualTo("ohjeung2");
+  }
+
+
 }
